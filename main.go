0790eebd1f49a34c328043aba7a911ab713d6258
Beata <beata@jasinski.com>
2024-06-23 14:57:35 -0700

    create printCommandEvents()

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/joho/godotenv"
 	"github.com/shomali11/slacker"
 )
+
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
+		fmt.Println("Commant Events")
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Event)
+		fmt.Println()
+	}
+}
 
 func main() {
 	godotenv.Load()
@@ -16,4 +28,6 @@ func main() {
 	os.Setenv("SLACK_APP_TOKEN", tokenapp)
 
 	bot := slacker.NewClient("SLACK_BOT_TOKEN", os.Getenv("SLACK_APP_TOKEN"))
+
+	go printCommandEvents(bot.CommandEvents())
 }
